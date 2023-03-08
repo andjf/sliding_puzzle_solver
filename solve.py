188955374f0ab72ba1035e511baaa521d37149d6
Andrew Ferrin <andjferrin@gmail.com>
2023-03-08 12:22:38 -0500

    Add ability to print moves
    Add the option, via "-m", "--moves", or "--moves-only", to print the
    tiles that should be moved on each move instead of the entire board
    being printed for each move.

--- solve.py
+++ solve.py
@@ -35,14 +35,14 @@ class Puzzle(object):
 
     @staticmethod
     def read_from_file(filename: str) -> Puzzle:
-        """Read and generate a board from a file"""
+        """Read and generate a puzzle from a file"""
         with open(filename, "r") as f:
             w, h = map(int, f.readline().split())
             return Puzzle([[int(v) for v in f.readline().split()] for _ in range(h)])
 
     @staticmethod
     def solved_of_size(n: int) -> Puzzle:
-        """Generate a solved board of size (n x n)"""
+        """Generate a solved puzzle of size (n x n)"""
         answer: List[List[int]] = []
         for i in range(n - 1):
             answer.append(list(range(i * n + 1, (i + 1) * n + 1)))
@@ -62,7 +62,7 @@ class Puzzle(object):
         self.y, self.x = y, x
 
     def next_moves(self) -> Generator[Puzzle]:
-        """Given the current state of the board, find all possible moves"""
+        """Given the current state of the puzzle, find all possible moves"""
         around: List[Tuple[int]] = [
             (self.x - 1, self.y),
             (self.x + 1, self.y),
@@ -73,11 +73,11 @@ class Puzzle(object):
         for nx, ny in around:
             if (0 <= nx < len(self.puzzle[0])) and (0 <= ny < len(self.puzzle)):
                 # Make a copy of the base puzzle
-                new_board = self.__copy__()
+                next_puzzle = self.__copy__()
                 # Perform the appropriate swap
-                new_board.move_into_empty(nx, ny)
-                # Yield the newly created board
-                yield new_board
+                next_puzzle.move_into_empty(nx, ny)
+                # Yield the newly created puzzle
+                yield next_puzzle
 
     def __copy__(self) -> Puzzle:
         """Create a deep copy of a Puzzle object"""
@@ -94,26 +94,26 @@ class Puzzle(object):
 class PuzzleSolver(object):
     def __init__(self, n: int) -> None:
         """Constructor for the PuzzleSolver class"""
-        # Generate a solved board of size (n x n)
+        # Generate a solved puzzle of size (n x n)
         solved: Puzzle = Puzzle.solved_of_size(n)
 
         # Initialize the solver dictionary.
-        # The value at a given board is it's parent board.
-        # This parent board is the board is the result of the transformation
-        # that should be made on the current board to optimally solve.
+        # The value at a given puzzle is it's parent puzzle.
+        # This parent puzzle is the puzzle is the result of the transformation
+        # that should be made on the current puzzle to optimally solve.
         self.solver: Dict[Tuple[Tuple[int]] : Tuple[Tuple[int]]] = dict()
-        # The solved board has no parent. It is already solved
+        # The solved puzzle has no parent. It is already solved
         self.solver[solved.to_tuple()] = None
 
-        # Start the search from the solved board
+        # Start the search from the solved puzzle
         q: Deque[Puzzle] = deque()
         q.append(solved)
 
-        # While there are more boards to be inspected, continue searching
+        # While there are more puzzles to be inspected, continue searching
         while len(q):
-            # Get the current board
+            # Get the current puzzle
             current_puzzle: Puzzle = q.popleft()
-            # Go through every possible next move for the current board
+            # Go through every possible next move for the current puzzle
             for next_puzzle in current_puzzle.next_moves():
                 # If there is a faster way to get to this puzzle state, skip it
                 if next_puzzle.to_tuple() not in self.solver:
@@ -124,20 +124,31 @@ class PuzzleSolver(object):
 
     @lru_cache(maxsize=None)
     def solve_steps(self, puzzle_tuple: Union[Tuple[Tuple[int]], None]) -> int:
-        """Given a board, get the number of steps to optimally solve that board"""
+        """Given a puzzle, get the number of steps to optimally solve that puzzle"""
         if puzzle_tuple not in self.solver:
             return -1
-        next_board = self.solver[puzzle_tuple]
-        return 1 + self.solve_steps(next_board) if next_board else 0
+        next_puzzle = self.solver[puzzle_tuple]
+        return 1 + self.solve_steps(next_puzzle) if next_puzzle else 0
+
+    def show_required_moves(self, puzzle_tuple: Tuple[Tuple[int]]) -> None:
+        """Given a puzzle, print the exact moves required to optimally solve"""
+        if puzzle_tuple not in self.solver:
+            print("Given puzzle is unsolvable.")
+            return
+        current_puzzle = puzzle_tuple
+        next_puzzle = self.solver[current_puzzle]
+        while next_puzzle != None:
+            print(get_diff(current_puzzle, next_puzzle))
+            current_puzzle, next_puzzle = next_puzzle, self.solver[next_puzzle]
 
     def show_solve_trace(self, puzzle_tuple: Union[Tuple[Tuple[int]], None]) -> int:
-        """Given a board, print the steps to optimally solve that board"""
-        # If this board is not reachable
+        """Given a puzzle, print the steps to optimally solve that puzzle"""
+        # If this puzzle is not reachable
         if puzzle_tuple not in self.solver:
             print("Given puzzle is unsolvable.")
             return -1
 
-        # Print the board out and a line separator
+        # Print the puzzle out and a line separator
         print(
             "\n".join(
                 " ".join(map(lambda v: f"{v:2d}" if v else "  ", row))
@@ -146,52 +157,53 @@ class PuzzleSolver(object):
         )
         print("-" * (len(puzzle_tuple) * 3 - 1))
 
-        next_board = self.solver[puzzle_tuple]
-        # If puzzle_tuple describes the solved board
-        if next_board == None:
+        next_puzzle = self.solver[puzzle_tuple]
+        # If puzzle_tuple describes the solved puzzle
+        if next_puzzle == None:
             # No moves need to be made
             return 0
 
-        # Otherwise, search this board's parent and count a move
-        return 1 + self.show_solve_trace(next_board)
+        # Otherwise, search this puzzle's parent and count a move
+        return 1 + self.show_solve_trace(next_puzzle)
 
 
-def main(filename: str) -> None:
+def get_diff(old_puzzle: Tuple[Tuple[int]], new_puzzle: Tuple[Tuple[int]]) -> int:
+    for old_row, new_row in zip(old_puzzle, new_puzzle):
+        for old_value, new_value in zip(old_row, new_row):
+            if old_value == 0:
+                return new_value
+            if new_value == 0:
+                return old_value
+    return -1
+
+
+def main(filename: str, show_moves_only: bool) -> None:
     puzzle = Puzzle.read_from_file(filename)
     solver = PuzzleSolver(len(puzzle.puzzle))
-    num_moves = solver.show_solve_trace(puzzle.to_tuple())
-    print(f"Solved in {num_moves} move{'s' * (num_moves != 1)}")
+    if show_moves_only:
+        solver.show_required_moves(puzzle.to_tuple())
+    else:
+        num_moves = solver.show_solve_trace(puzzle.to_tuple())
+        print(f"Solved in {num_moves} move{'s' * (num_moves != 1)}")
 
 
 def display_frequencies():
     frequencies: DefaultDict[List:int] = defaultdict(list)
 
     puzzle_solver: PuzzleSolver = PuzzleSolver(3)
-    for board_tuple in puzzle_solver.solver:
-        frequencies[puzzle_solver.solve_steps(board_tuple)].append(board_tuple)
+    for puzzle_tuple in puzzle_solver.solver:
+        frequencies[puzzle_solver.solve_steps(puzzle_tuple)].append(puzzle_tuple)
 
-    total_boards: int = 0
+    total_puzzles: int = 0
     for frequency in frequencies:
         print(f"{frequency:2d}: {len(frequencies[frequency]):5d}")
-        total_boards += len(frequencies[frequency])
+        total_puzzles += len(frequencies[frequency])
 
-    print("\nMost difficult boards:")
-    print("-" * 9)
-
-    for hard_board in frequencies[31]:
-        print(
-            "\n".join(
-                " ".join(map(lambda v: f"{v:2d}" if v else "  ", row))
-                for row in hard_board
-            )
-        )
-        print("-" * 8)
-
-    print("Total:", total_boards)
+    print("Total:", total_puzzles)
 
 
 if __name__ == "__main__":
-    if len(argv) == 2:
-        main(argv[1])
+    if 2 <= len(argv) <= 3:
+        main(argv[1], len(argv) == 3 and argv[2] in ["-m", "--moves", "--moves-only"])
     else:
         display_frequencies()
